3a1f6ae1580682af801069552959bb7f0f616b7c
Utkarsh Rawat <debugomega@gmail.com>
2026-02-07 19:59:32 +0530

    googlescopes added and libraries added

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from flask import Flask
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, redirect, request, render_template, session
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.credentials import Credentials
+from PyPDF2 import PdfReader
+import os
+import io
+import re
+
+app = Flask(__name__)
+app.secret_key = "dev_secret_key"
+
+# Allow HTTP for local development
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+SCOPES = [
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/classroom.courses.readonly",
+    "https://www.googleapis.com/auth/classroom.student-submissions.students.readonly",
+    "https://www.googleapis.com/auth/drive.readonly"
+]
 
 app = Flask(__name__)
 
