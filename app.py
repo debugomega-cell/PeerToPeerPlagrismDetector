5e69c54230da9042e6aa19d24f4176619a518472
Utkarsh Rawat <debugomega@gmail.com>
2026-02-07 20:02:00 +0530

    scopes and libraries added

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, request, render_template, session
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from flask import Flask, redirect, request, render_template, session
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google.oauth2.credentials import Credentials
 from PyPDF2 import PdfReader
+
 import os
 import io
 import re
 
+
+# ---------- APP SETUP ----------
+
 app = Flask(__name__)
 app.secret_key = "dev_secret_key"
 
-# Allow HTTP for local development
+# Allow HTTP during local testing
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+
+# ---------- GOOGLE SCOPES ----------
 
 SCOPES = [
     "openid",
@@ -25,12 +31,3 @@ SCOPES = [
     "https://www.googleapis.com/auth/classroom.student-submissions.students.readonly",
     "https://www.googleapis.com/auth/drive.readonly"
 ]
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Plagiarism Checker Running!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
